851f3cc92ec1ff7037c7009d59c9bc2c252267b5
MathTeacherSmith <Clarence.Smith@vertexacademies.org>
2026-02-02 19:12:38 +0000

    Solved code

--- module.py
+++ module.py
@@ -2,14 +2,17 @@
 
 class BankAccount:
     def __init__(self, owner, balance):
-        self.owner = "owner"
+        self.owner = owner
         self.balance = balance
 
     def greet(self):
         print(f"Welcome, {self.owner}! Your balance is ${self.balance}.")
 
     def deposit(self, amount):
-        self.balance + amount
+        self.balance = self.balance + amount
 
     def withdraw(self, amount):
-        self.balance = self.balance - amount
+        if self.balance >= amount:
+            self.balance = self.balance - amount
+        else:
+            print("Insufficient funds. Withdrawal canceled.")
